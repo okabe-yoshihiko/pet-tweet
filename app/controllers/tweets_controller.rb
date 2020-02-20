17e367d7452bdd5c09c11da6821c9911d2f1152d
okabe-yoshihiko <skr.ys@icloud.com>
2020-02-20 20:04:31 +0900

    css

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,5 +7,16 @@ class TweetsController < ApplicationController
   def new
     @tweets = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :text)
+  
+  end
+
   
 end
