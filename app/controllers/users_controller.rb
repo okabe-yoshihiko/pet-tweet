6cf99f1c375407ce21d4ea6ef0f5b4257b4d02e0
okabe-yoshihiko <skr.ys@icloud.com>
2020-03-03 12:05:28 +0900

    master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @tweets = user.tweets
     @image = user.image
     @profile = user.profile
+    @user  = User.find(params[:id])
   end
 
   def edit
